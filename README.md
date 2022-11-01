56a8edbb8fe09a990e5abf312d25ac66f4a3948c
Anthony53Nguyen <81620655+Anthony53Nguyen@users.noreply.github.com>
2022-11-01 19:15:22 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# My second React HW
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import BaiTapThucHanhLayout from './BaiTapLayoutComponent/BaiTapThucHanhLayout';
+import BaiTapGlasses from './BiaTapState_Glasses/BaiTapGlasses';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render (
+
+  <div>
+    {/* <BaiTapThucHanhLayout /> */}
+    
+    <BaiTapGlasses/>
+
+  </div>
+
+)
+
